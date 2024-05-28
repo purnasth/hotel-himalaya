28f1d7075a876a2e2684488020b1b75074a183c8
Purna Shrestha <crush9918@gmail.com>
2024-05-28 22:01:08 +0545

    feat: Create hall component page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,10 +6,11 @@ const Banner = ({ banner, title, description, page }) => {
   return (
     <div className="responsive-banner relative w-full h-80 md:h-full lg:h-screen">
       <img src={banner} alt={title} className="w-full h-full object-cover" />
+      <div className="absolute inset-0 size-full bg-gradient-to-t from-black/50 to-black/20"></div>
       <div className="absolute left-0 bottom-0 text-center bg-gradient-to-t from-black/80 to-black/0 w-full py-4">
-        <div className="max-w-lg text-gradient mx-auto space-y-4 py-12">
+        <div className="max-w-xl text-gradient mx-auto space-y-4 py-12">
           <h3 className="text-4xl leading-snug">{title}</h3>
-          <p className="text-sm">{description}</p>
+          <p className="px-8 text-sm">{description}</p>
         </div>
         <BreadCrumb page={page} />
       </div>
--- src/pages/HallPage.jsx
+++ src/pages/HallPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import EnquiryNow from "../components/ui/EnquiryNow";
+import Banner from "../components/Banner";
 
 const HallPage = () => {
-
   return (
     <>
-      THis is enquiry
+      <Banner
+        banner="https://s3-alpha-sig.figma.com/img/4e1d/2131/7f8b9696b68e3c854fbc935f98175f3d?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pA1NFVAtiFIAJat8m01RW86Rc-RXKxVyoYwyNn~nLCgbFjBtYQSiV2PLBnRI3J85D9~-kkS486KrblYHJ1-HRdoy2a1k5EYCLAKNjVOHRNmv0CrCExL906Hsocla79v-GB18waWVbbAgS6OrXSuobpJ7YJwOHUlJYNi7xod3hDaaiSsI0k6vZy2z75lSD3pAuh-FJ-zYqHHl34eqZY~OzQ1SlcA7MMJo41NfS42qLj314BZyGBkkUDEcR~Q1l8Sh7y3Ivr4b0ie7H2XPSflWp8Pensq3GCTnw7dbCNuD9R0DfkCJ81-i89dYJu4hCyGs5x8vzYSBm9CL-wvY1iWFfQ__"
+        title="Auspicious Convention Center"
+        description="Comfortable accommodation directly in the hotel with extensive 
+        Wellness and exceptional gastronomy."
+        page="Events"
+      />
       <EnquiryNow />
     </>
   );
