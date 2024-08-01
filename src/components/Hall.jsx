7cc2cfe53598be0dd4d550189fbb20ed00913170
Purna Shrestha <crush9918@gmail.com>
2024-08-01 08:25:57 +0545

    responsive: complete hall section

--- src/components/Hall.jsx
+++ src/components/Hall.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { withDataFetching, Slider } from "../constants/data";
+import { Link } from "react-router-dom";
 
 const Hall = ({ data }) => {
   const { title, slogan, details, forCategories, buttonText, hallImages } =
@@ -9,13 +10,18 @@ const Hall = ({ data }) => {
   return (
     <>
       <main className="pb-0 px-0">
-        <div className="container px-24 pb-24">
-          <div className="text-center space-y-8 px-12">
-            <span className="uppercase">{slogan}</span>
-            <h3 className="text-5xl leading-snug">{title}</h3>
-            <p className="text-base">{details}</p>
+        <div className="md:container md:px-24 pb-24">
+          <div className="text-left md:text-center space-y-5 md:space-y-8 px-6 sm:px-12">
+            <span className="text-xs md:text-base uppercase">{slogan}</span>
+
+            <h3 className="text-xl sm:text-2xl md:text-4xl lg:text-5xl leading-snug lg:leading-snug">
+              {title}
+            </h3>
+            <p className="text-sm text-justify md:text-base md:text-center">
+              {details}
+            </p>
           </div>
-          <div className="my-24 grid grid-cols-1 md:grid-cols-3 gap-12">
+          <div className="hidden md:grid px-6 md:px-0 my-24 grid-cols-1 md:grid-cols-3 gap-12">
             {forCategories.map((category, index) => (
               <div key={index} className="space-y-6">
                 <h4 className="text-2xl font-medium">{category.title}</h4>
@@ -25,18 +31,21 @@ const Hall = ({ data }) => {
               </div>
             ))}
           </div>
-          <div className="text-center">
-            <button className="bg-gradient px-6 py-2 rounded-full hover-outline">
+          <div className="mt-8 md:pt-0 text-center">
+            <Link
+              to="/events"
+              className="bg-gradient px-6 py-2 rounded-full hover-outline"
+            >
               {buttonText}
-            </button>
+            </Link>
           </div>
         </div>
 
         <Slider
           slides={hallImages}
-          containerClassName="w-full h-[95vh]"
+          containerClassName="w-full h-72 sm:h-[50vh] sm:min-h-96 lg:h-[95vh]"
           buttonClassName="hall-slider-button"
-          imgClassName="w-full h-full object-cover absolute inset-0 transition-all duration-[2s]"
+          imgClassName="w-full h-72 sm:h-[50vh] sm:min-h-96 lg:h-[95vh] object-cover absolute inset-0 transition-all duration-[2s]"
           trueClassName="opacity-100 translate-y-0"
           falseClassName="opacity-0 translate-y-16 -z-10"
         />
