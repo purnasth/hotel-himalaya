b9cbfdad3230a07f202edb3e96064576a07628fc
Purna Shrestha <crush9918@gmail.com>
2024-08-03 10:32:22 +0545

    responsive[beautiful]: Complete recreation page responsive

--- src/components/RecreationLayout.jsx
+++ src/components/RecreationLayout.jsx
@@ -3,91 +3,63 @@ import React from "react";
 const RecreationLayout = ({ recreationsData }) => {
   return (
     <>
-      <section className="mt-4 p-8 py-0">
+      <section className="mt-24 xl:mt-0 xl:p-8 xl:pt-0 py-0">
         {recreationsData.map((recreation, index) => (
           <div
-            className="grid grid-cols-3 content-center gap-16 mt-28"
+            className="grid grid-cols-1 xl:grid-cols-3 content-center gap-y-12 xl:gap-16 xl:p-8"
             key={index}
           >
-            {index % 2 === 0 ? (
-              <>
-                <div className="col-span-1 content-center w-full">
-                  <h3 className="text-3xl leading-snug">{recreation.title}</h3>
-                  <p className="text-justify md:text-pretty text-base mt-4 mb-20">
-                    {recreation.description}
-                  </p>
-                  <h3 className="text-2xl mb-6">Facilities</h3>
-                  <ul className="flex items-center gap-6 flex-wrap">
-                    {recreation.facilities.map((facility, facilityIndex) => (
-                      <li
-                        key={facilityIndex}
-                        className={`px-4 py-2 rounded-2xl relative overflow-hidden`}
-                        style={{ backgroundColor: recreation.facilitiesColor }}
-                      >
-                        <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-black/20 opacity-30 -z-10 pointer-events-none"></div>
-                        {facility}
-                      </li>
-                    ))}
-                  </ul>
+            <div
+              className={`col-span-1 content-center w-full ${
+                index % 2 === 0 ? "" : "xl:order-last"
+              }`}
+            >
+              <h3 className="text-xl md:text-2xl xl:text-3xl md:leading-normal xl:leading-snug">
+                {recreation.title}
+              </h3>
+              <p className="text-sm text-justify md:text-base mt-4 mb-8 md:mb-20">
+                {recreation.description}
+              </p>
+              <h3 className="text-xl md:text-2xl mb-6">Facilities</h3>
+
+              <ul className="flex items-center gap-3 md:gap-6 flex-wrap">
+                {recreation.facilities.map((facility, facilityIndex) => (
+                  <li
+                    key={facilityIndex}
+                    className={`px-4 py-2 rounded-2xl relative overflow-hidden text-xs md:text-base`}
+                    style={{ backgroundColor: recreation.facilitiesColor }}
+                  >
+                    <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-black/20 opacity-30 -z-10 pointer-events-none"></div>
+                    {facility}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div
+              className={`col-span-2 grid grid-cols-5 gap-4 md:gap-8 ${
+                index % 2 === 0 ? "" : "xl:order-first"
+              } md:mt-16 xl:mt-24 ${
+                index === recreationsData.length - 1
+                  ? "mb-0"
+                  : "mb-28 lg:mb-44 xl:mb-6"
+              }`}
+            >
+              {recreation.images.map((image, imageIndex) => (
+                <div
+                  key={imageIndex}
+                  className={`col-span-${image.colSpan || 1} ${
+                    imageIndex % 2 === 0 ? "content-end" : ""
+                  }`}
+                >
+                  <img
+                    src={image.url}
+                    alt={image.alt}
+                    className="rounded-2xl"
+                    draggable="false"
+                  />
                 </div>
-                <div className="col-span-2 grid grid-cols-5 gap-8 p-8">
-                  {recreation.images.map((image, imageIndex) => (
-                    <div
-                      key={imageIndex}
-                      className={`col-span-${image.colSpan || 1} ${
-                        imageIndex % 2 === 0 ? "content-end" : ""
-                      }`}
-                    >
-                      <img
-                        src={image.url}
-                        alt={image.alt}
-                        className="rounded-2xl"
-                        draggable="false"
-                      />
-                    </div>
-                  ))}
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="col-span-2 grid grid-cols-5 gap-8 p-8">
-                  {recreation.images.map((image, imageIndex) => (
-                    <div
-                      key={imageIndex}
-                      className={`col-span-${image.colSpan || 1} ${
-                        imageIndex % 2 === 0 ? "content-end" : ""
-                      }`}
-                    >
-                      <img
-                        src={image.url}
-                        alt={image.alt}
-                        className="rounded-2xl"
-                        draggable="false"
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className="col-span-1 content-center w-full">
-                  <h3 className="text-3xl leading-snug">{recreation.title}</h3>
-                  <p className="text-justify md:text-pretty text-base mt-4 mb-20">
-                    {recreation.description}
-                  </p>
-                  <h3 className="text-2xl mb-6">Facilities</h3>
-                  <ul className="flex items-center gap-6 flex-wrap">
-                    {recreation.facilities.map((facility, facilityIndex) => (
-                      <li
-                        key={facilityIndex}
-                        className={`px-4 py-2 rounded-2xl relative overflow-hidden`}
-                        style={{ backgroundColor: recreation.facilitiesColor }}
-                      >
-                        <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-black/20 opacity-30 -z-10 pointer-events-none"></div>
-                        {facility}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </>
-            )}
+              ))}
+            </div>
           </div>
         ))}
       </section>
